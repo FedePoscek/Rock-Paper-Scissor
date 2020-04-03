c5ce0cf53fb0b6f61b28b13600568b2035cd8d6a
Federico Poscek <53795030+FedePoscek@users.noreply.github.com>
2020-04-03 18:52:14 -0300

    Icons Animations

--- js/RPS-app.js
+++ js/RPS-app.js
@@ -1,136 +1,134 @@
-// var opciones = ["piedra", "papel", "tijera"];
-var opciones = [0, 1, 2];
+// var options = ["piedra", "papel", "tijera"];
+
+var options = [0, 1, 2];
 var choiceMachine;
 var counterHuman = 1;
 var counterMachine = 1;
 
-function aleatorio(min, max){
-    var numero = Math.floor(Math.random() * (max - min +1) + min);
-    return numero;
+function randomNumber(min, max){
+    var number = Math.floor(Math.random() * (max - min +1) + min);
+    return number;
 }
 
 function Humano(choiceHuman) {
+
     choiceHuman = parseInt(choiceHuman);
-    choiceMachine = aleatorio(0,2);
+    choiceMachine = randomNumber(0,2);
+
+    var counter = document.getElementById('textoResultado');
+    var counterM = document.getElementById('counterM');
+    var counterH = document.getElementById('counterH');
+    var numberCounterM = document.getElementById('numberCounterM');
+    var numberCounterH = document.getElementById('numberCounterH');
+    var finalBackground = document.getElementById('finalScreen');
+    var user = document.getElementById('user');
+    var robot = document.getElementById('robot');
+
     document.getElementById('descriptiveText').innerHTML = '(click in one of 3 options to continue playing!)';
 
-    if(choiceHuman == 0) { // humano eligió piedra 
-        document.getElementById('user').src='svg/rockstar_main.svg';
-        
-        if (opciones[choiceMachine] == 1) { // maquina eligio papel 
-            var counter = document.getElementById('textoResultado');
-            counter.innerHTML = 'YOU LOST!';
-            counter.style.color = 'var(--rojo)';
-            document.getElementById('counterM').innerHTML = 'MACHINE';
-            document.getElementById('numberCounterM').innerHTML = counterMachine++;
-            document.getElementById('robot').src='svg/toilet-paper_main.svg';
-        } else {
-            if (opciones[choiceMachine] == 2) { // maquina eligio tijera 
-                var counter = document.getElementById('textoResultado');
-                counter.innerHTML = 'YOU WON!';
-                counter.style.color = 'var(--verde)';
-                document.getElementById('counterH').innerHTML = 'HUMAN';
-                document.getElementById('numberCounterH').innerHTML = counterHuman++;
-                document.getElementById('robot').src='svg/scissor_main.svg';
-            } else {
-                if (opciones[choiceMachine] == 0) { // maquina eligio piedra 
-                    var counter = document.getElementById('textoResultado');
-                    counter.innerHTML = 'IT\'S A TIE!';
-                    counter.style.color = 'var(--blanco)';
-                    document.getElementById('robot').src='svg/rockstar_main.svg';
-                }
-            }
-        } 
-    }
-
-    if (choiceHuman == 1) { // humano eligió papel 
-        document.getElementById('user').src='svg/toilet-paper_main.svg';
-        if (opciones[choiceMachine] == 2) { // máquina eligió tijera 
-            var counter = document.getElementById('textoResultado');
-            counter.innerHTML = 'YOU LOST!';
-            counter.style.color = 'var(--rojo)';
-            document.getElementById('counterM').innerHTML = 'MACHINE';
-            document.getElementById('numberCounterM').innerHTML = counterMachine++;
-            document.getElementById('robot').src='svg/scissor_main.svg';
-        } else {
-            if (opciones[choiceMachine] == 0) { // máquina eligió piedra 
-                var counter = document.getElementById('textoResultado');
-                counter.innerHTML = 'YOU WON!';
-                counter.style.color = 'var(--verde)';
-                document.getElementById('counterH').innerHTML = 'HUMAN';
-                document.getElementById('numberCounterH').innerHTML = counterHuman++;
-                document.getElementById('robot').src='svg/rockstar_main.svg';
-            } else {
-                if (opciones[choiceMachine] == 1) { // máquina eligió papel 
-                    var counter = document.getElementById('textoResultado');
-                    counter.innerHTML = 'IT\'S A TIE!';
-                    counter.style.color = 'var(--blanco)';
-                    document.getElementById('robot').src='svg/toilet-paper_main.svg';
-                }
-            }
-        }
-    }
-
-    if (choiceHuman == 2) { // humano eligió tijera 
-        document.getElementById('user').src='svg/scissor_main.svg';
-        if (opciones[choiceMachine] == 1) { // máquina eligió papel 
-            var counter = document.getElementById('textoResultado');
-                counter.innerHTML = 'YOU WON!';
-                counter.style.color = 'var(--verde)';
-                document.getElementById('counterH').innerHTML = 'HUMAN';
-                document.getElementById('numberCounterH').innerHTML = counterHuman++;
-                document.getElementById('robot').src='svg/toilet-paper_main.svg';
-        
-        } else {
-            if (opciones[choiceMachine] == 0) { // máquina eligió piedra 
-                var counter = document.getElementById('textoResultado');
+        if(choiceHuman == 0) { // humano eligió piedra 
+            user.src='svg/rockstar_main.svg';
+            if (options[choiceMachine] == 1) { // máquina eligió papel 
                 counter.innerHTML = 'YOU LOST!';
                 counter.style.color = 'var(--rojo)';
-                document.getElementById('counterM').innerHTML = 'MACHINE';
-                document.getElementById('numberCounterM').innerHTML = counterMachine++;
-                document.getElementById('robot').src='svg/rockstar_main.svg';
- 
+                counterM.innerHTML = 'MACHINE';
+                numberCounterM.innerHTML = counterMachine++;
+                robot.src='svg/toilet-paper_main.svg';
             } else {
-                if (opciones[choiceMachine] == 2) { // máquina eligió tijera 
-                    var counter = document.getElementById('textoResultado');
-                    counter.innerHTML = 'IT\'S A TIE!';
-                    counter.style.color = 'var(--blanco)';
-                    document.getElementById('robot').src='svg/scissor_main.svg';
+                if (options[choiceMachine] == 2) { // máquina eligió  tijera 
+                    counter.innerHTML = 'YOU WON!';
+                    counter.style.color = 'var(--verde)';
+                    counterH.innerHTML = 'HUMAN';
+                    numberCounterH.innerHTML = counterHuman++;
+                    robot.src='svg/scissor_main.svg';
+                } else {
+                    if (options[choiceMachine] == 0) { // máquina eligió piedra 
+                        counter.innerHTML = 'IT\'S A TIE!';
+                        counter.style.color = 'var(--blanco)';
+                        robot.src='svg/rockstar_main.svg';
+                    }
+                }
+            } 
+        }
+
+        if (choiceHuman == 1) { // humano eligió papel 
+            user.src='svg/toilet-paper_main.svg';
+            if (options[choiceMachine] == 2) { // máquina eligió tijera 
+                counter.innerHTML = 'YOU LOST!';
+                counter.style.color = 'var(--rojo)';
+                counterM.innerHTML = 'MACHINE';
+                numberCounterM.innerHTML = counterMachine++;
+                robot.src='svg/scissor_main.svg';
+            } else {
+                if (options[choiceMachine] == 0) { // máquina eligió piedra 
+                    counter.innerHTML = 'YOU WON!';
+                    counter.style.color = 'var(--verde)';
+                    counterH.innerHTML = 'HUMAN';
+                    numberCounterH.innerHTML = counterHuman++;
+                    robot.src='svg/rockstar_main.svg';
+                } else {
+                    if (options[choiceMachine] == 1) { // máquina eligió papel 
+                        counter.innerHTML = 'IT\'S A TIE!';
+                        counter.style.color = 'var(--blanco)';
+                        robot.src='svg/toilet-paper_main.svg';
+                    }
                 }
             }
         }
-    }
-    if (counterHuman > counterMachine) {
-        document.getElementById('counterH').style.color = 'var(--verde)';
-        document.getElementById('counterM').style.color = 'var(--rojo)';
-        document.getElementById('numberCounterH').style.color = 'var(--verde)';
-        document.getElementById('numberCounterM').style.color = 'var(--rojo)';
-    } else {
-        if (counterHuman < counterMachine) {
-            document.getElementById('counterH').style.color = 'var(--rojo)';
-            document.getElementById('counterM').style.color = 'var(--verde)';
-            document.getElementById('numberCounterH').style.color = 'var(--rojo)';
-            document.getElementById('numberCounterM').style.color = 'var(--verde)';
-    } else {
-        document.getElementById('counterH').style.color = 'var(--blanco)';
-        document.getElementById('counterM').style.color = 'var(--blanco)';
-        document.getElementById('numberCounterH').style.color = 'var(--blanco)';
-        document.getElementById('numberCounterM').style.color = 'var(--blanco)';
-    }
-    };
-    if (counterHuman > 3) {
-            var finalBackground = document.getElementById('finalScreen');
-            finalBackground.style.display = 'block';
-            finalBackground.style.backgroundColor = 'rgba(var(--verde-RGB), 0.8)';
-            finalBackground.innerHTML = '<h1>YOU WON!!</h1>' + '<br><h2>' + 'Human: ' + (counterHuman - 1) + '<br>Machine: ' + (counterMachine - 1) + '<h2>' + '<h4>Click anywhere to play again!</h4>';;
-    } else {
-        if (counterMachine > 3) {
-            var finalBackground = document.getElementById('finalScreen');
-            finalBackground.style.display = 'block';
-            finalBackground.style.backgroundColor = 'rgba(var(--rojo-RGB), 0.8)';
-            finalBackground.innerHTML = '<h1>YOU LOST!!</h1>' + '<br><h2>' + 'Human: ' + (counterHuman - 1) + '<br>Machine: ' + (counterMachine - 1) + '<h2>' + '<h3>Click anywhere to play again!</h3>';
+
+        if (choiceHuman == 2) { // humano eligió tijera 
+            user.src='svg/scissor_main.svg';
+            if (options[choiceMachine] == 1) { // máquina eligió papel 
+                    counter.innerHTML = 'YOU WON!';
+                    counter.style.color = 'var(--verde)';
+                    counterH.innerHTML = 'HUMAN';
+                    numberCounterH.innerHTML = counterHuman++;
+                    robot.src='svg/toilet-paper_main.svg';
+            } else {
+                if (options[choiceMachine] == 0) { // máquina eligió piedra 
+                    counter.innerHTML = 'YOU LOST!';
+                    counter.style.color = 'var(--rojo)';
+                    counterM.innerHTML = 'MACHINE';
+                    numberCounterM.innerHTML = counterMachine++;
+                    robot.src='svg/rockstar_main.svg';
+                } else {
+                    if (options[choiceMachine] == 2) { // máquina eligió tijera 
+                        counter.innerHTML = 'IT\'S A TIE!';
+                        counter.style.color = 'var(--blanco)';
+                        robot.src='svg/scissor_main.svg';
+                    }
+                }
+            }
         }
-    }
+        if (counterHuman > counterMachine) {
+            counterH.style.color = 'var(--verde)';
+            counterM.style.color = 'var(--rojo)';
+            numberCounterH.style.color = 'var(--verde)';
+            numberCounterM.style.color = 'var(--rojo)';
+        } else {
+            if (counterHuman < counterMachine) {
+                counterH.style.color = 'var(--rojo)';
+                counterM.style.color = 'var(--verde)';
+                numberCounterH.style.color = 'var(--rojo)';
+                numberCounterM.style.color = 'var(--verde)';
+        } else {
+            counterH.style.color = 'var(--blanco)';
+            counterM.style.color = 'var(--blanco)';
+            numberCounterH.style.color = 'var(--blanco)';
+            numberCounterM.style.color = 'var(--blanco)';
+        }
+        };
+        if (counterHuman > 3) {
+                finalBackground.style.display = 'block';
+                finalBackground.style.backgroundColor = 'rgba(var(--verde-RGB), 0.8)';
+                finalBackground.innerHTML = '<h1>YOU WON!!</h1>' + '<br><h2>' + 'Human: ' + (counterHuman - 1) + '<br>Machine: ' + (counterMachine - 1) + '<h2>' + '<h4>Click anywhere to play again!</h4>';;
+        } else {
+            if (counterMachine > 3) {
+                finalBackground.style.display = 'block';
+                finalBackground.style.backgroundColor = 'rgba(var(--rojo-RGB), 0.8)';
+                finalBackground.innerHTML = '<h1>YOU LOST!!</h1>' + '<br><h2>' + 'Human: ' + (counterHuman - 1) + '<br>Machine: ' + (counterMachine - 1) + '<h2>' + '<h3>Click anywhere to play again!</h3>';
+            }
+        }
 }
 function functionFinalScreen() {
     location.reload();
