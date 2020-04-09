7d336c61c4db2374a7cf903cb7635ad0b3f23838
Federico Poscek <53795030+FedePoscek@users.noreply.github.com>
2020-04-09 19:59:36 -0300

    Updates Templates, etc

--- js/RPS-app.js
+++ js/RPS-app.js
@@ -1,9 +1,9 @@
 // var options = ["piedra", "papel", "tijera"];
 
-var options = [0, 1, 2];
-var choiceMachine;
-var counterHuman = 1;
-var counterMachine = 1;
+var options = [0, 1, 2],
+    choiceMachine,
+    counterHuman = 0,
+    counterMachine = 0;
 
 function randomNumber(min, max){
     var number = Math.floor(Math.random() * (max - min +1) + min);
@@ -12,17 +12,17 @@ function randomNumber(min, max){
 
 function Humano(choiceHuman) {
 
-    choiceHuman = parseInt(choiceHuman);
     choiceMachine = randomNumber(0,2);
 
-    var counter = document.getElementById('textoResultado');
-    var counterM = document.getElementById('counterM');
-    var counterH = document.getElementById('counterH');
-    var numberCounterM = document.getElementById('numberCounterM');
-    var numberCounterH = document.getElementById('numberCounterH');
-    var finalBackground = document.getElementById('finalScreen');
-    var user = document.getElementById('user');
-    var robot = document.getElementById('robot');
+    var counter = document.getElementById('textoResultado'),
+        counterM = document.getElementById('counterM'),
+        counterH = document.getElementById('counterH'),
+        numberCounterM = document.getElementById('numberCounterM'),
+        numberCounterH = document.getElementById('numberCounterH'),
+        user = document.getElementById('user'),
+        robot = document.getElementById('robot'),
+        finalBackground = document.getElementById('finalScreen');
+
 
     document.getElementById('descriptiveText').textContent = '(click in one of 3 options to continue playing!)';
 
@@ -32,14 +32,14 @@ function Humano(choiceHuman) {
                 counter.textContent = 'YOU LOST!';
                 counter.style.color = 'var(--rojo)';
                 counterM.textContent = 'MACHINE';
-                numberCounterM.textContent = counterMachine++;
+                numberCounterM.textContent = ++counterMachine;
                 robot.src='svg/toilet-paper_main.svg';
             } else {
                 if (options[choiceMachine] == 2) { // máquina eligió  tijera 
                     counter.textContent = 'YOU WON!';
                     counter.style.color = 'var(--verde)';
                     counterH.textContent = 'HUMAN';
-                    numberCounterH.textContent = counterHuman++;
+                    numberCounterH.textContent = ++counterHuman;
                     robot.src='svg/scissor_main.svg';
                 } else {
                     if (options[choiceMachine] == 0) { // máquina eligió piedra 
@@ -57,14 +57,14 @@ function Humano(choiceHuman) {
                 counter.textContent = 'YOU LOST!';
                 counter.style.color = 'var(--rojo)';
                 counterM.textContent = 'MACHINE';
-                numberCounterM.textContent = counterMachine++;
+                numberCounterM.textContent = ++counterMachine;
                 robot.src='svg/scissor_main.svg';
             } else {
                 if (options[choiceMachine] == 0) { // máquina eligió piedra 
                     counter.textContent = 'YOU WON!';
                     counter.style.color = 'var(--verde)';
                     counterH.textContent = 'HUMAN';
-                    numberCounterH.textContent = counterHuman++;
+                    numberCounterH.textContent = ++counterHuman;
                     robot.src='svg/rockstar_main.svg';
                 } else {
                     if (options[choiceMachine] == 1) { // máquina eligió papel 
@@ -82,14 +82,14 @@ function Humano(choiceHuman) {
                     counter.textContent = 'YOU WON!';
                     counter.style.color = 'var(--verde)';
                     counterH.textContent = 'HUMAN';
-                    numberCounterH.textContent = counterHuman++;
+                    numberCounterH.textContent = ++counterHuman;
                     robot.src='svg/toilet-paper_main.svg';
             } else {
                 if (options[choiceMachine] == 0) { // máquina eligió piedra 
                     counter.textContent = 'YOU LOST!';
                     counter.style.color = 'var(--rojo)';
                     counterM.textContent = 'MACHINE';
-                    numberCounterM.textContent = counterMachine++;
+                    numberCounterM.textContent = ++counterMachine;
                     robot.src='svg/rockstar_main.svg';
                 } else {
                     if (options[choiceMachine] == 2) { // máquina eligió tijera 
@@ -100,6 +100,7 @@ function Humano(choiceHuman) {
                 }
             }
         }
+
         if (counterHuman > counterMachine) {
             counterH.style.color = 'var(--verde)';
             counterM.style.color = 'var(--rojo)';
@@ -118,15 +119,31 @@ function Humano(choiceHuman) {
             numberCounterM.style.color = 'var(--blanco)';
         }
         };
-        if (counterHuman > 3) {
+        
+        if (counterHuman >= 3) {
                 finalBackground.style.display = 'block';
                 finalBackground.style.backgroundColor = 'rgba(var(--verde-RGB), 0.8)';
-                finalBackground.innerHTML = '<h1>YOU WON!!</h1>' + '<br><h2>' + 'Human: ' + (counterHuman - 1) + '<br>Machine: ' + (counterMachine - 1) + '<h2>' + '<h4>Click anywhere to play again!</h4>';;
-        } else {
-            if (counterMachine > 3) {
+
+                // con JavaScript Template Literals
+                finalBackground.innerHTML = `
+                    <h1>YOU WON!!</h1>
+                    <h2>Human: ${(counterHuman)}</h2>
+                    <h2>Machine: ${(counterMachine)}</h2>
+                    <h4>Click anywhere to play again!</h4>
+                    `;
+                    
+            } else {
+            if (counterMachine >= 3) {
                 finalBackground.style.display = 'block';
                 finalBackground.style.backgroundColor = 'rgba(var(--rojo-RGB), 0.8)';
-                finalBackground.innerHTML = '<h1>YOU LOST!!</h1>' + '<br><h2>' + 'Human: ' + (counterHuman - 1) + '<br>Machine: ' + (counterMachine - 1) + '<h2>' + '<h3>Click anywhere to play again!</h3>';
+
+                // con JavaScript Template Literals
+                finalBackground.innerHTML = `
+                    <h1>YOU LOST!!</h1>
+                    <h2>Human: ${(counterHuman)}</h2>
+                    <h2>Machine: ${(counterMachine)}</h2>
+                    <h3>Click anywhere to play again!</h3>
+                    `;
             }
         }
 }
