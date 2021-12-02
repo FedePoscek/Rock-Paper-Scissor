e2e1bfb737c47babcad59bdaf94bd1f9aafa8a13
Fede <fposcek@gmail.com>
2021-12-02 10:24:49 -0300

    Changes: Name register, No name register, animations on icons, img for inputs and addEventListener

--- js/RPS-app.js
+++ js/RPS-app.js
@@ -1,22 +1,7 @@
-// función onload para chequear que haya un usuario jugando (solo para "rematch..."")
-window.onload = function Splash(event) {
-    event.preventDefault();
-
-    if (sessionStorage.getItem("idUser")) {
-        document.getElementById('popup_register').style.display = "none";
-        let div = document.createElement('div');
-            div.innerHTML = sessionStorage.getItem("idUser");
-            counterH.appendChild(div);
-    } else {
-        console.log("NO hay ningún nombre guardado");
-    }
-};
-
-
 // REFERENCIA DE VARIABLES
-// var options = ["piedra", "papel", "tijera"];
-// counterM == Counter Machine
-// counterH == Counter Human
+// var options = [0 = "piedra", 1 = "papel", 2 = "tijera"];
+// counterM == Counter Machine (número de vueltas ganadas por la máquina)
+// counterH == Counter Human (número de vueltas ganadas por el usuario)
 // numberCounterM == Numero del contador Machine
 // numberCounterH == Numero del contador Humano
 // accumulatedUser == Array que acumula las iteraciones del Humano y las traduce en la placa final
@@ -26,6 +11,7 @@ var options = [0, 1, 2],
     accumulatedUser = [],
     accumulatedMachine = [],
     choiceMachine,
+    choiceHuman,
     counterHuman = 0,
     counterMachine = 0,
     counter = document.getElementById('textoResultado'),
@@ -33,25 +19,159 @@ var options = [0, 1, 2],
     counterH = document.getElementById('counterH'),
     numberCounterM = document.getElementById('numberCounterM'),
     numberCounterH = document.getElementById('numberCounterH'),
-    noNamePlayer = sessionStorage.getItem("idUser") || "HUMAN",
+    noNamePlayer,
     namePlayer,
     user = document.getElementById('user'),
-    robot = document.getElementById('robot');
-    
+    robot = document.getElementById('robot'),
+    rock = document.getElementById("piedra"),
+    paper = document.getElementById("papel"),
+    scissor = document.getElementById("tijera"),
+    start = document.getElementById('popUp_container_boton'),
+    startSecondChance = document.getElementById("boton_enter_secondChance");
+    startSecondChance.style.display = "none";
 // ------ finalización de declaración de variables globales
 
 
-// función sigue jugando haciendo click en "rematch..."
-function currentGame() {
-    location.reload();
-    console.log("Seguimos!");
+// Creo los addEventListener para cada uno de los inputs de "LET'S PLAY", "LET'S PLAY Second chance", Rock, Paper y Scissor
+start.addEventListener("click", function(event){
+    event.preventDefault();
+    idHumanPlayer();
+});
+
+startSecondChance.addEventListener("click", function(event){
+    event.preventDefault();
+    idHumanPlayer_secondChance();
+});
+
+rock.addEventListener("click", function(event){
+    event.preventDefault();
+    deactivateButtons();
+    Humano(0); // humano eligió piedra 
+});
+
+paper.addEventListener("click", function(event){
+    event.preventDefault();
+    deactivateButtons();
+    Humano(1); // humano eligió papel 
+});
+
+scissor.addEventListener("click", function(event){
+    event.preventDefault();
+    deactivateButtons();
+    Humano(2); // humano eligió tijera 
+});
+
+
+// función para la generación del nombre del nuevo jugador
+function idHumanPlayer() {
+
+    namePlayer = (document.getElementById('inputName').value).trim().toUpperCase();
+
+    if (namePlayer){
+        document.getElementById('popup_register').style.display = "none";
+        let div = document.createElement('div');
+        div.innerHTML = `${namePlayer}`;
+        counterH.appendChild(div);
+
+            console.log(namePlayer);
+
+    }  else if (!namePlayer){
+
+        let noValidationName = document.getElementById('cambio');
+        noValidationName.textContent = "REALLY?? I dare you to do it again!";
+        start.style.display = "none";
+        startSecondChance.style.display = "flex"; //cambio de botón
+        
+        console.log("primera chance NO");
+    } 
+} 
+
+// función para la generación de la chance de nuevo nombre o "HUMAN" en caso de no haber nada.
+function idHumanPlayer_secondChance() {
+
+    namePlayer = (document.getElementById('inputName').value).trim().toUpperCase();
+
+    if (namePlayer){
+        document.getElementById('popup_register').style.display = "none";
+        let div = document.createElement('div');
+        div.innerHTML = `${namePlayer}`;
+        counterH.appendChild(div);
+
+            console.log(namePlayer);
+
+    } else if (!namePlayer){
+        // si no hay ningún nombre, tras dos intentos, muestra un cartel temporal donde indica que se llamará "HUMAN" al jugador.
+        noNamePlayer = "HUMANOID";
+        let noValidationName = document.getElementById('cambio');
+
+        noValidationName.textContent = "NO NAME??, OK! ... then I'll call you: 'HUMANOID'";
+        document.getElementById('popup_text_secondary').style.display = "none";
+        document.getElementById('inputName').style.display = "none";
+        startSecondChance.style.display = "none";
+
+        setTimeout(function(){
+        document.getElementById('popup_register').style.display = "none";
+        let div = document.createElement('div');
+        div.innerHTML = `${noNamePlayer}`;
+        counterH.appendChild(div);
+            console.log(noNamePlayer);
+        }, 2000);        
+    } 
 }
+
+
+// desactivo los botones en cada evento para evitar el doble click y que loopee la animación
+function deactivateButtons() {
+    rock.disabled = true;
+    paper.disabled = true;
+    scissor.disabled = true;
+}
+
+// activo los botones en cada evento que quedaron desactivados con deactivateButtons().
+function ActivateButtons() {
+    rock.disabled = false;
+    paper.disabled = false;
+    scissor.disabled = false;
+}
+
+// función sigue jugando haciendo click en "rematch..."
+// opción para NO recargar la página y volver todos los datos a 0 menos el nombre
+function currentGame() {
+    accumulatedUser.length = 0,
+    accumulatedMachine.length = 0,
+    counterHuman = 0,
+    counterMachine = 0,
+    numberCounterM.innerHTML = 0,
+    numberCounterH.innerHTML = 0,
+    finalBackground = document.getElementById('finalScreen'),
+    finalBackground.style.display = 'none',
+    counter.innerHTML = 
+        `
+        <div class="footerGrid";>
+            <div id="textoResultado" class="textResultAnimated" style="font-size: 100%;";>CHOOSE:
+                <img class="buttonText" src="svg/rockstar_yellow.svg" alt="Rockstar Icon">
+                <img class="buttonText" src="svg/toilet-paper_yellow.svg" alt="Toilet Paper Icon">
+                <img class="buttonText" src="svg/scissor_yellow.svg" alt="Scissors Icon">?</div>
+        </div>
+                `,
+    counterH.style.color = 'var(--violeta-medio)',
+    counterM.style.color = 'var(--violeta-medio)',
+    numberCounterH.style.color = 'var(--violeta-oscuro-degrade)',
+    numberCounterM.style.color = 'var(--violeta-oscuro-degrade)',
+    user.src='svg/user_main.svg',
+    robot.src='svg/robot_main.svg';
+    CounterAnimation(user);
+    CounterAnimation(robot);
+    ActivateButtons();
+        console.log("Seguimos!");
+
+}
+
 
 // función para recargar la pantalla y comenzar otra vez
 function newGame() {
-    console.log("Arrancamos otra vez!");
-    sessionStorage.removeItem("idUser");
     location.reload();
+        console.log("Arrancamos otra vez!");
 }
         
 
@@ -69,7 +189,7 @@ function animationTie(element) {
         { transform: 'rotate(0deg) scale(1)' },
     ], { 
         // tiempos
-        duration: 400,
+        duration: 300,
         iterations: 1,
         easing: 'linear', 
     }
@@ -80,21 +200,18 @@ function animationWon(element) {
     element.animate(
     [
         // keyframes
-        { transform: 'translateY(0px)' }, 
-        { transform: 'translateY(-10px)' },
-        { transform: 'translateY(-35px)' },
-        { transform: 'translateY(-5px)' },
-        { transform: 'translateY(-10px)' },
-        { transform: 'translateY(-5px)' },
-        { transform: 'translateY(-2px)' },
-        { transform: 'translateY(-4px)' },
-        { transform: 'translateY(-2px)' },
-        { transform: 'translateY(-1px)' }
+        { transform: 'scale(1.4, 1.4) translateY(0)' },
+        { transform: 'scale(1.1, 0.9) translateY(0)' },
+        { transform: 'scale(0.9, 1.1) translateY(-100px)' },
+        { transform: 'scale(1.05, 0.95) translateY(0)' },
+        { transform: 'scale(1, 1) translateY(-7px)' },
+        { transform: 'scale(1, 1) translateY(0)' },
+        { transform: 'scale(1, 1) translateY(0)' },
     ], { 
         // tiempos
-        duration: 500,
+        duration: 600,
         iterations: 1,
-        easing: 'ease', 
+        easing: 'ease-out', 
     }
 )};
 
@@ -113,26 +230,22 @@ function CounterAnimation(element) {
     });
 }
 
+// Array para cargar los iconos que rotan
+var imagesIcons = new Array(
+    ['svg/toilet-paper_main.svg'],
+    ['svg/scissor_main.svg'],
+    ['svg/rockstar_main.svg'],
+    // ['svg/user_main.svg'],
+    // ['svg/robot_main.svg'],
+    ['svg/spock_main.svg']
+);
 
-// función para la generación del nombre del nuevo jugador
-function idHumanPlayer() {
-   
-    namePlayer = (document.getElementById('inputName').value).toUpperCase();
-    sessionStorage.setItem("idUser", namePlayer);
-    console.log(`sessionStorage = ${sessionStorage.getItem("idUser")}`);
-// Creo un div para cargar el nombre del jugador y si no hay nada va "Human"
-
-    let div = document.createElement('div');
-        if (namePlayer){
-            div.innerHTML = `${namePlayer}`;
-            counterH.appendChild(div);
-            console.log(namePlayer);
-        } else {
-            div.innerHTML = `${noNamePlayer}`;
-            counterH.appendChild(div);
-            console.log(noNamePlayer);
-        };
+function rotateImagesIcons(){
+    var index = Math.floor((Math.random() * imagesIcons.length));
+    document.getElementById("user").src = imagesIcons[index][0];
+    document.getElementById("robot").src = imagesIcons[index][0];
 }
+
 
 // función para random de elección máquina
 function randomNumber(min, max){
@@ -140,41 +253,54 @@ function randomNumber(min, max){
         return number;
 }
 
-// función para elección humano
+
+// función según la elección humano
 function Humano(choiceHuman) {
-    
-        namePlayer = (document.getElementById('inputName').value).toUpperCase();
+   
+    namePlayer = (document.getElementById('inputName').value).toUpperCase();
 
-            //funciones para cambios de icono Machine
-            robotPaper = function() {
-                    robot.src='svg/toilet-paper_main.svg';
-                },
-            robotScissor = function() {
-                    robot.src='svg/scissor_main.svg';
-                },
-            robotRock = function() {
-                    robot.src='svg/rockstar_main.svg';
-                },
+        //funciones para cambios de icono Machine - si no los defino como función siempre muestra la imagen de la última variable (en este caso: rock);
+        robotPaper = function() {
+                robot.src='svg/toilet-paper_main.svg';
+            },
+        robotScissor = function() {
+                robot.src='svg/scissor_main.svg';
+            },
+        robotRock = function() {
+                robot.src='svg/rockstar_main.svg';
+            },
 
-             //funciones para cambios de icono User
-            userPaper = function() {
-                    user.src='svg/toilet-paper_main.svg';
-                },
-            userScissor = function() {
-                    user.src='svg/scissor_main.svg';
-                },
-            userRock = function() {
-                    user.src='svg/rockstar_main.svg';
-                },
+            //funciones para cambios de icono User
+        userPaper = function() {
+                user.src='svg/toilet-paper_main.svg';
+            },
+        userScissor = function() {
+                user.src='svg/scissor_main.svg';
+            },
+        userRock = function() {
+                user.src='svg/rockstar_main.svg';
+            },
 
-            continueText = function() {
-                document.getElementById('descriptiveText').textContent = '(click in one of 3 options to continue playing!)';
-            };
+        continueText = function() {
+            document.getElementById('descriptiveText').textContent = '(click in one of 3 options to continue playing!)';
+        };
 
+    // la máquina define un número random y su consecuente opción
+    choiceMachine = randomNumber(0,2);
+    // animación de las opciones   
+
+    function pararAnimacion() {
+        clearInterval(fin);
+    }
+
+    rotateImagesIcons();
+    fin = setInterval(rotateImagesIcons,35);
+    setTimeout(pararAnimacion, 400);
 
     // función para definir si gana Roca, Papel o Tijera y sus consecuencias
-    choiceMachine = randomNumber(0,2);
-            //este prirmer if es para frenar la función (e inhabilitar los botones) cuando el contador llegue a 3.
+    // el setTimeout espera a la animación de las opciones
+    setTimeout(function(){
+        //este prirmer if es para frenar la función (e inhabilitar los botones) cuando el contador llegue a 3.
         if(counterHuman === 3 || counterMachine === 3){
             return;
         }   
@@ -252,6 +378,12 @@ function Humano(choiceHuman) {
         }
             console.log("usuario: " + accumulatedUser); //testeo en consola
             console.log("maquina: " + accumulatedMachine); //testeo en consola
+
+            // reavtivo los botones luego de qeu termina la función para poder volver a hacer click
+            rock.disabled = false;
+            paper.disabled = false;
+            scissor.disabled = false;
+    } ,600);
 }
 
 
@@ -290,7 +422,6 @@ function youWon() {
     numberCounterH.textContent = ++counterHuman;
     ColorCounter();
     CounterResults();
-
         setTimeout(function(){
             counter.textContent = 'YOU\'RE IN LUCK!!';
             counter.style.color = 'var(--verde)';
@@ -304,7 +435,6 @@ function youLost() {
     numberCounterM.textContent = ++counterMachine;
     ColorCounter();
     CounterResults();
-
         setTimeout(function(){
             counter.textContent = 'WANNA REMATCH??';
             counter.style.color = 'var(--rojo)';
@@ -316,7 +446,6 @@ function itsATie() {
     counter.style.color = 'var(--blanco)';
     ColorCounter();
     CounterResults();
-
         setTimeout(function(){
             counter.textContent = 'TIE THIS OFF!!';
             counter.style.color = 'var(--blanco)';
@@ -324,17 +453,18 @@ function itsATie() {
 }
 
 
+// función para realizar el listado de resultados de las partidas
+function SummaryResults(){
+    var result = accumulatedUser.map( (item, ix) => item + accumulatedMachine[ix]+`<br>`);
+    console.log("resultado: " + result);
+    return `${result}`.replace(/,/g,"");
+}
+
+
 // función para pantalla final y contador total de ganados y perdidos
 function CounterResults() {
 
     var finalBackground = document.getElementById('finalScreen');
-    
-    // función para realizar el listado de resultados de las partidas
-    function SummaryResults(){
-        var result = accumulatedUser.map( (item, ix) => item + accumulatedMachine[ix]+`<br>`);
-        console.log(result);
-        return `${result}`.replace(/,/g,"");
-    }
 
     if (counterHuman === 3) {
             setTimeout(function(){
@@ -345,7 +475,7 @@ function CounterResults() {
                 <h2>${namePlayer || noNamePlayer}: ${counterHuman}</h2>
                 <h2>MACHINE: ${counterMachine}</h2>
                 <h5>${SummaryResults()}</h5>
-                <button class="boton" onclick="currentGame()">WANNA REMATCH, ${sessionStorage.getItem("idUser")}?</button>
+                <button class="boton" onclick="currentGame()">WANNA REMATCH, ${namePlayer || noNamePlayer}?</button>
                 <br>
                 <button class="boton" onclick="newGame()">NEW PLAYER!</button>
                 `;
@@ -360,7 +490,7 @@ function CounterResults() {
                 <h2>${namePlayer || noNamePlayer}: ${counterHuman}</h2>
                 <h2>MACHINE: ${counterMachine}</h2>
                 <h5>${SummaryResults()}</h5>
-                <button class="boton" onclick="currentGame()">WANNA REMATCH, ${sessionStorage.getItem("idUser")}?</button>
+                <button class="boton" onclick="currentGame()">WANNA REMATCH, ${namePlayer || noNamePlayer}?</button>
                 <br>
                 <button class="boton" onclick="newGame()">NEW PLAYER!</button>
                 `;
